d21377f72e0c9fd3cf49c13a9e18a3e4165cce23
Raghav-Kamath <raghavkamath4@gmail.com>
2024-02-07 23:34:05 +0530

    add executable,py file, requirements file

---
+++ language_translator.py
@@ -0,0 +1,53 @@
+# pip install googletrans==3.1.0a0 
+from tkinter import *
+from tkinter import ttk
+from googletrans import Translator, LANGUAGES
+
+root = Tk()
+root.geometry('1080x400')
+
+root.resizable(0,0)
+root.config(bg = 'ghost white')
+
+root.title("Language Translator")
+
+Label(root, text = "LANGUAGE TRANSLATOR", font = "arial 20 bold", bg='white smoke',foreground='#1a7031').pack()
+
+Label(root,text ="Choose the input language", font = 'arial 13 bold', bg ='white smoke').place(x=20,y=30)
+
+Label(root,text ="Choose the output language", font = 'arial 13 bold', bg ='white smoke').place(x=830,y=30)
+
+Label(root,text ="Default languages chosen are English to Hindi", font = 'arial 15 bold', bg ='white smoke' , width = '100').pack(side = 'bottom')
+
+Label(root,text ="Enter Text", font = 'arial 13 bold', bg ='white smoke').place(x=200,y=100)
+
+Input_text = Text(root,font = 'arial 10', height = 11, wrap = WORD, padx=5, pady=5, width = 60)
+Input_text.place(x=30,y = 150)
+
+Label(root,text ="Output", font = 'arial 13 bold', bg ='white smoke').place(x=780,y=100)
+
+Output_text = Text(root,font = 'arial 10', height = 11, wrap = WORD, padx=5, pady= 5, width =60)
+Output_text.place(x = 600 , y = 150)
+
+language = list(LANGUAGES.values())
+
+src_lang = ttk.Combobox(root, values= language, width =22)
+src_lang.place(x=20,y=60)
+src_lang.set('english')
+
+dest_lang = ttk.Combobox(root, values= language, width =22)
+dest_lang.place(x=890,y=60)
+dest_lang.set('hindi')
+
+def Translate():
+    translator = Translator()
+    translated=translator.translate(text= Input_text.get(1.0, END) , src = src_lang.get(), dest = dest_lang.get())
+
+    Output_text.delete(1.0, END)
+    Output_text.insert(END, translated.text)
+
+trans_btn = Button(root, text = 'Translate',font = 'arial 12 bold',pady = 5,command = Translate , bg = 'royal blue1', activebackground = 'sky blue')
+
+trans_btn.place(x = 490, y= 180 )
+
+root.mainloop()
